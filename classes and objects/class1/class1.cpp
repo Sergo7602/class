d9a09b2d3b8ec1b807c29ea1fa37e0e09d8f4de2
Sergo7602 <100104366+Sergo7602@users.noreply.github.com>
2022-11-18 23:33:43 +1000

    Add files via upload

---
+++ classes and objects/class1/class1.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+using namespace std;
+class Calcurator {
+private:
+    int num1;
+    int num2;
+
+public:
+    /*  Calcurator(int a, int b)
+      {
+          this->num1 = a;
+          this->num2 = b;
+      }*/
+    bool set_num1(double num1) {
+        this->num1 = num1;
+        if (this->num1 == 0) {
+            // cout << "\nНеверный ввод!";
+            return false;
+        }
+        else {
+
+            return true;
+        }
+    }
+    bool set_num2(double num2) {
+        this->num2 = num2;
+        if (this->num2 == 0) {
+            // cout << "\nНеверный ввод!";
+            return false;
+        }
+        else {
+
+            return true;
+        }
+    }
+    void add() {
+        double res;
+        res = num1 + num2;
+        cout << "\nnum1 + num2=" << res;
+
+        //  return 0;
+    }
+    void multiply() {
+        double res;
+        res = num1 * num2;
+        cout << "\nnum1 * num2=" << res;
+        ;
+        // return 0;
+    }
+    void subtract_1_2() {
+        double res;
+        res = num2 - num1;
+        cout << "\nnum2 - num1=" << res;
+        ;
+        //  return 0;
+    }
+    void subtract_2_1() {
+        double res;
+        res = num1 - num2;
+        cout << "\nnum1 - num2=" << res;
+        //  return 1;
+    }
+    void divide_1_2() {
+        double res;
+        res = num1 / num2;
+        cout << "\nnum1 / num2=" << res;
+        //  return 0;
+    }
+    void divide_2_1() {
+        double res;
+        res = num2 / num1;
+        cout << "\nnum2 / num1=" << res;
+        // return 0;
+    }
+};
+
+int main() {
+    int a, b;
+    Calcurator calc;
+    cout << "Введите num1: ";
+    cin >> a;
+    cout << "\nВведие num2: ";
+    cin >> b;
+    calc.set_num1(a);
+    if (calc.set_num1(a) == false) {
+        cout << "\nНеверный ввод!";
+        do
+        {
+            cout << "Введите num1: ";
+            cin >> a;
+        } while (!calc.set_num1(a));
+
+        // return 0;
+    }
+    calc.set_num2(b);
+    if (calc.set_num2(b) == false) {
+        cout << "\nНеверный ввод!";
+        do
+        {
+            cout << "\nВведите num2: ";
+            cin >> b;
+        } while (!calc.set_num2(b));
+        // return 0;
+    }
+    calc.add();
+    calc.multiply();
+    calc.subtract_1_2();
+    calc.subtract_2_1();
+    calc.divide_1_2();
+    calc.divide_2_1();
+}
---
+++ classes and objects/class2/class2.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+using namespace std;
+class Counter
+{
+private:
+    int count;
+public:
+    void count_set(int l)
+    {
+        this->count = l;
+    }
+    Counter()
+    {
+        this->count = 1;
+    }
+    void Plus()
+    {
+        count++;
+    }
+    void Minus()
+    {
+        count--;
+    }
+    void Res()
+    {
+        cout << count;
+        cout << endl;
+    }
+    /*void Exit(bool temp)
+    {
+      cout<<"\nДо свидания!";
+      temp=false;
+    }*/
+};
+int main()
+{
+    cout << "Вы хотите указать начальное значение счётчика? Введите да или нет:\n";
+    string text;
+    Counter calc;
+    int n;
+    char s;
+    char m;
+    cin >> text;
+    if (text == "да")
+    {
+        cin >> n;
+        calc.count_set(n);
+    }
+    else
+    {
+        n = 1;
+    }
+    bool temp = true;
+    while (temp)
+    {
+        cout << "Введите команду ('+', '-', '=' или 'x')";
+        cin >> s;
+        switch (s)
+        {
+        case '+':
+            calc.Plus();
+            break;
+        case '-':
+            calc.Minus();
+            break;
+        case '=':
+            calc.Res();
+            break;
+        case 'x':
+            //calc.Exit(temp);
+            temp = false;
+            cout << "\nДо свидания!";
+            break;
+        }
+    }
+    return 0;
+}
